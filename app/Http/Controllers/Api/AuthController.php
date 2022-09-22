e6d858f539ff1b1755d914a2dbcc7806b56be0e9
Min Thu Zaw <minthuzaw775@gmail.com>
2022-09-22 10:30:12 +0630

    passport api access_token modified

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -39,7 +39,7 @@ class AuthController extends Controller
         if ($user) {
             if (Hash::check($attributes['password'], $user->password)) {
                 $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-                $response = ['token' => $token];
+                $response = ['access_token' => $token];
                 return response($response, 200);
             } else {
                 $response = ["message" => "Password mismatch"];
