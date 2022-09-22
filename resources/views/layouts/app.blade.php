2854329096adde57f0c5e1b79c6a5ee57a994736
Min Thu Zaw <minthuzaw775@gmail.com>
2022-09-22 16:16:00 +0630

    favicon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="shortcut icon" href="{{ asset('images/blog.png') }}">
     <title>{{ config('app.name', 'Blog') }}</title>
 
     <!-- Scripts -->
