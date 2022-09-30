b4d6b27602b2b0ff433361e9f08b273d41198d8d
Min Thu Zaw <minthuzaw775@gmail.com>
2022-09-30 10:32:10 +0630

    fixed conflit

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,24 @@
 
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CommentController;
-use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect('login');
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::resource('/articles', ArticleController::class)->except(['edit', 'update']);
+
+    Route::post('/comments', [CommentController::class, 'store'])->name('comments.store')->middleware('auth');
+    Route::delete('/comments/delete/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+
+    Route::resource('/profiles', ProfileController::class)->except(['show', 'store', 'create', 'destroy']);
+
+    Route::get('/2y$10$KMfnFfmsOTiehOIbJIyKesxrcvkqpl1aU9vlsqunTLcsgDug/{n}', [ArticleController::class, 'qr'])->name('qr');
+
 });
 
-Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
-//Route::get('/articles', [HomeController::class, 'index'])->name('articles.index');
-
-Route::get('/articles/show/{article}', [ArticleController::class, 'show'])->name('articles.show');
-
-Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create')->middleware('auth');
-Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
-
-Route::delete('/articles/delete/{article}', [ArticleController::class, 'destroy'])->name('articles.destroy');
-
-Route::post('/comments', [CommentController::class, 'store'])->name('comments.store')->middleware('auth');
-Route::get('/comments/delete/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
-
-Route::get('/2y$10$KMfnFfmsOTiehOIbJIyKesxrcvkqpl1aU9vlsqunTLcsgDug/{n}', [ArticleController::class, 'qr'])->name('qr');
-Auth::routes();
+Auth::routes(['verify' => true]);
