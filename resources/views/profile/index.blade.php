221b063275c747975f6b08951898130a5fb73b5d
Min Thu Zaw <minthuzaw775@gmail.com>
2022-10-20 21:06:48 +0630

    bug fixed profile edit

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -14,7 +14,7 @@
                     <div class="card-body text-center">
                         <img src="{{ $user->profile ? 'images/'.$user->profile : asset('favicon/user.png') }}"
                              class="rounded-circle border-2 shadow-lg" alt="Profile_image"
-                             style="width: 100%;height: 100%">
+                             style="width: 130px;height: 130px">
                         <h1 class="mt-4">{{ ucfirst($user->name) }}</h1>
                         <h4 class="my-3">{{ $user->email }}</h4>
                         <p class="m-0">{{ $user->phone_no }}</p>
