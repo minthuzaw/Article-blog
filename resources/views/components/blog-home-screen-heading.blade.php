7424c8814fdf49c50b5551ebcae7b4496c3b9d31
Min Thu Zaw <minthuzaw775@gmail.com>
2023-03-10 09:37:19 +0630

    added: search query

--- resources/views/components/blog-home-screen-heading.blade.php
+++ resources/views/components/blog-home-screen-heading.blade.php
@@ -1,5 +1,5 @@
 {{--heading--}}
 <div class="heading-text mt-2">
-    <span class="heading-color text-center mb-3">Dream Collector's </span>
+    <span class="gradient-text text-center mb-3">Dream Collector's </span>
     <span class="text-black semi-bold-600">Articles</span>
 </div>
