0e3025593eefb0654a2140482a4d3196d27ad6b3
Min Thu Zaw <minthuzaw775@gmail.com>
2023-03-12 15:06:50 +0630

    fix: back to top btn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,50 +80,22 @@
 
 </div>
 
-<script>
-    //Get the button
-    let mybutton = document.getElementById("btn-back-to-top");
-
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-        scrollFunction();
-    };
-
-    function scrollFunction() {
-        if (
-            document.body.scrollTop > 20 ||
-            document.documentElement.scrollTop > 20
-        ) {
-            mybutton.style.display = "block";
-        } else {
-            mybutton.style.display = "none";
-        }
-    }
-
-    // When the user clicks on the button, scroll to the top of the document
-    mybutton.addEventListener("click", backToTop);
-
-    function backToTop() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
-</script>
-
+<script src="{{ asset('js/btn-back-to-top.js') }}"></script>
 <script src="{{ asset('js/isotope.pkgd.js') }}"></script>
 <script src="{{ asset('js/jquery.min.js') }}"></script>
 @stack('js-content')
-<script>
-    /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function () {
-        var currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos) {
-            document.getElementById("navbar").style.top = "0";
-        } else {
-            document.getElementById("navbar").style.top = "-60px";
-        }
-        prevScrollpos = currentScrollPos;
-    }
-</script>
+{{--<script>--}}
+{{--    /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */--}}
+{{--    var prevScrollpos = window.pageYOffset;--}}
+{{--    window.onscroll = function () {--}}
+{{--        var currentScrollPos = window.pageYOffset;--}}
+{{--        if (prevScrollpos > currentScrollPos) {--}}
+{{--            document.getElementById("navbar").style.top = "0";--}}
+{{--        } else {--}}
+{{--            document.getElementById("navbar").style.top = "-60px";--}}
+{{--        }--}}
+{{--        prevScrollpos = currentScrollPos;--}}
+{{--    }--}}
+{{--</script>--}}
 </body>
 </html>
